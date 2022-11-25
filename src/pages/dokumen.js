20852c1f7430982776f4aed72525ae2164a640fd
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-25 18:11:44 +0700

    change loading color

--- src/pages/dokumen.js
+++ src/pages/dokumen.js
@@ -257,7 +257,7 @@ const Document = () => {
                         <div role="status">
                           <svg
                             aria-hidden="true"
-                            class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            class="mr-2 w-8 h-8 text-white animate-spin dark:text-white fill-blue-600"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
